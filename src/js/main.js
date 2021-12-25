67cd4cb0c7a96917026aaf6ced1e28c55b8a661c
Guilherme Ribeiro <augusto.gui.2208@gmail.com>
2021-12-25 17:15:32 -0300

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,25 @@
 const openMenu = document.querySelector(".btn-mobile");
-const navBarMobile = document.querySelector(".mobile-nav")
+const navBarMobile = document.querySelector(".mobile-nav");
+const closeMenu = document.querySelectorAll(".mobile-nav__menu--a");
+const body = document.querySelector("body");
 
-openMenu.addEventListener("click", () => {
-  navBarMobile.classList.toggle("is-open")
-})
+function handleButtonMobile() {
+  navBarMobile.classList.toggle("is-open");
+  const containClass = navBarMobile.classList.contains("is-open");
+  if (containClass) {
+    body.classList.toggle("is-open");
+  }
+}
+
+function handleCloseMenuClick() {
+  closeMenu.forEach(close => {
+    console.log(close)
+    close.addEventListener("click", () => {
+      navBarMobile.classList.remove("is-open");
+      body.classList.remove("is-open");
+    });
+  });
+}
+handleCloseMenuClick()
+
+openMenu.addEventListener("click", handleButtonMobile);
